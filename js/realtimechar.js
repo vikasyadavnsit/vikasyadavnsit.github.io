bdb56bcf4ebe6363a2ee277a0e3e4d72b8669bcd
vikasyadavnsit <vickytauruss@gmail.com>
2018-01-31 03:30:52 +0530

    m,

---
+++ js/realtimechar.js
@@ -0,0 +1,50 @@
+
+window.onload = function () {
+
+var dataPoints = [];
+
+var options = {
+	theme: "light2",
+	title: {
+		text: "Live Chart from JSON Data"
+	},
+	data: [{
+		type: "line",
+    markerType: "square",
+    color: "#F08080",
+
+		dataPoints: dataPoints
+	}]
+};
+
+$("#chartContainer2").CanvasJSChart(options);
+updateData();
+
+// Initial Values
+var xValue = 0;
+var yValue = 10;
+var newDataCount = 6;
+
+function addData(data) {
+	if (newDataCount != 1) {
+		$.each(data, function (key, value) {
+			dataPoints.push({ x: value[0], y: parseInt(value[1]) });
+			xValue++;
+			yValue = parseInt(value[1]);
+		});
+		newDataCount = 1;
+	} else {
+		//dataPoints.shift();
+		dataPoints.push({ x: data[0][0], y: parseInt(data[0][1]) });
+		xValue++;
+		yValue = parseInt(data[0][1]);
+	}
+	$("#chartContainer2").CanvasJSChart().render();
+	setTimeout(updateData, 500);
+}
+
+function updateData() {
+	$.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + xValue + "&ystart=" + yValue + "&length=" + newDataCount + "type=json", addData);
+}
+
+}
